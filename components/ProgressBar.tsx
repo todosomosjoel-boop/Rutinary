b0b981473f5065fd0a3aa7aca0ae5e216e882720
todosomosjoel-boop <todosomosjoel@gmail.com>
2026-08-30 23:59:47 -0400

    Nuevas mejoras

--- components/ProgressBar.tsx
+++
@@ -1,4 +0,0 @@
-export default function ProgressBar({ value }: { value: number }) {
-  const safe = Math.max(0, Math.min(100, value));
-  return <div className="progress"><span style={{ width: `${safe}%` }} /></div>;
-}
